b257c0405b2e960d2e8f038725a0cba5e84fc0c3
Jackson Lenhart <jacksonlenhart@gmail.com>
2019-01-12 18:06:31 -0500

    Coordinate game initialization and resumption/real-time updating w/ web sockets. Deal initial holdem hands

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const WebSocket = require('ws');
 
+const { generateDeck, extractRandomCard } = require('./poker');
+
 const app = express();
 
 const wss = new WebSocket.Server({ port: 9090 });
 
-// Only one table for now with a 10,000 buyin
+// Only one table for now with a 10,000 buyin.
+// Will probably use a message queue for this eventually
+// rather than having the server be stateful.
 const players = [];
 const hands = [];
-const buyin = 100000
-let handInProgress = false;
+const buyin = 100000;
+let deck = generateDeck();
 
 app.listen(8080, function() {
   console.log('Listening on port 8080...');
@@ -38,9 +42,25 @@ wss.on('connection', function(ws) {
           ws.send(JSON.stringify(loginFailureData));
         }
         else {
-          players.push({ username: messageData.username, stack: buyin });
-          const loginData = { type: 'login', username: messageData.username };
+          const player = { username: messageData.username, stack: buyin, ready: false };
+          // A copy of players before adding the new one will be the opponents
+          // of the player being added.
+          const opponents = players.slice();
+          players.push(player);
+          ws.username = player.username;
+
+          const loginData = { type: 'login', player, opponents };
           ws.send(JSON.stringify(loginData));
+
+          const joinData = { type: 'join', playerJoined: player };
+          const joinDataSerialized = JSON.stringify(joinData);
+
+          // Broadcast to rest of clients that someone joined
+          wss.clients.forEach(function(client) {
+            if (client !== ws) {
+              client.send(joinDataSerialized);
+            }
+          });
         }
         break;
 
@@ -48,7 +68,12 @@ wss.on('connection', function(ws) {
       case 'gateway':
         const player = players.find(p => p.username === messageData.username);
         if (player) {
-          const gatewayData = { type: 'gateway', player };
+          const opponents = players.filter(p => p.username !== player.username);
+          // Note: The only way of determining whether we are in the middle of
+          // hand right now is whether or not finding the hand returns null.
+          const hand = hands.find(h => h.username === player.username);
+
+          const gatewayData = { type: 'gateway', player, opponents, hand: hand.hand };
           ws.send(JSON.stringify(gatewayData));
         }
         else {
@@ -59,45 +84,35 @@ wss.on('connection', function(ws) {
           ws.send(JSON.stringify(gatewayFailureData));
         }
         break;
+
+      case 'ready':
+        for (const p of players) {
+          if (p.username === messageData.username) {
+            p.ready = true;
+          }
+        }
+
+        // If everyone is ready, deal out some hands
+        if (!players.some(p => p.ready === false)) {
+          let player;
+          let hand;
+          wss.clients.forEach(function(client) {
+            player = players.find(p => p.username === client.username);
+            if (player) {
+              // Just playing Texas Hold 'em for now. (Starting with 2 cards)
+              hand = [extractRandomCard(deck), extractRandomCard(deck)];
+              hands.push({ username: player.username, hand });
+
+              client.send(JSON.stringify({ type: 'hand', hand }));
+            }
+          });
+        }
+        break;
+
       default:
         console.error('Unrecognized message type:', messageData.type);
     }
   });
-
-  /*app.get('/gateway/:username', function(req, res) {
-    const username = req.params.username.toLowerCase();
-
-    if (usernames.includes(username)) {
-      // We are logged in
-      if (username in activeSessions === false) {
-        activeSessions[username] = true;
-      }
-
-      res.sendStatus(200);
-    }
-    else {
-      // Oooohhh!! suspicious!
-      res.sendStatus(401);
-    }
-  });*/
-
-  /*players.push({ id: uuid(), stack: buyin });
-
-  if (players.length === 1) {
-    ws.send('Waiting on others to join');
-  }
-  else if (players.length >= 2 && !handInProgress) {
-    wss.clients.forEach(function(client) {
-      client.send('New player joined. Starting now!');
-    });
-
-    handInProgress = true;
-  }
-  else if (players.length >= 2) {
-    wss.clients.forEach(function(client) {
-      client.send('New player joined!');
-    });
-  }*/
 
   // DEBUG:
   console.log(players);
