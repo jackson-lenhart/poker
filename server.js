e677511eb0217e7ac34b4dfdeaf24a6781a64675
Jackson Lenhart <jacksonlenhart@gmail.com>
2019-01-24 01:16:58 -0500

    Migrate over to socket.io and add in call event and react to end of pre-flop betting round.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const WebSocket = require('ws');
+const http = require('http');
+const socketio = require('socket.io');
 
 const { generateDeck, extractRandomCard } = require('./poker');
 
 const app = express();
-
-const wss = new WebSocket.Server({ port: 9090 });
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Only one table for now with a 10,000 buyin.
 // Will probably use a message queue for this eventually
@@ -21,12 +22,21 @@ let bigBlindIndex = 0;
 let smallBlindIndex = 1;
 let actionIndex;
 let pot = 0;
-// 'bets' array will be reset to empty after each round of betting.
+// 'contributions' array will be reset to empty after each round of betting.
 // It is mainly to keep track of bets made before a raise
-let bets = [];
+let contributions = [];
+let board = [];
 
-app.listen(8080, function() {
-  console.log('Listening on port 8080...');
+const stages = ['lobby', 'pre-flop', 'flop', 'turn', 'river', 'showdown'];
+let stageIndex = 0;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
 });
 
 app.use(express.static('public'));
@@ -35,161 +45,199 @@ app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
-wss.on('connection', function(ws) {
-  ws.on('message', function(message) {
-    const messageData = JSON.parse(message);
+io.on('connection', function(client) {
+  client.on('login', function(username) {
+    if (players.some(p => p.username === username)) {
+      client.emit('login-failure', `User ${username} already exists.`);
+    }
+    else {
+      const player = { username, stack: buyin, ready: false };
+      // A copy of players before adding the new one will be the opponents
+      // of the player being added.
+      players.push(player);
+      client.username = player.username;
 
-    switch (messageData.type) {
+      client.emit('login-success', { player, players });
 
-      // This is effectively a signup and login combined (lol)
-      case 'login':
-        // we just reject if the username already exists
-        if (players.some(p => p.username === messageData.username)) {
-          const loginFailureData = {
-            type: 'login-failure',
-            msg: `User ${messageData.username} already exists.`
-          };
-          ws.send(JSON.stringify(loginFailureData));
-        }
-        else {
-          const player = { username: messageData.username, stack: buyin, ready: false };
-          // A copy of players before adding the new one will be the opponents
-          // of the player being added.
-          players.push(player);
-          ws.username = player.username;
+      client.broadcast.emit('join', players);
+    }
+  });
 
-          const loginData = { type: 'login', player, players };
-          ws.send(JSON.stringify(loginData));
+  client.on('gateway', function(username) {
+    let playerFound = false;
+    for (let i = 0; i < players.length; i++) {
+      if (players[i].username === username) {
+        playerFound = true;
+        client.username = username;
 
-          const joinData = { type: 'join', players };
-          const joinDataSerialized = JSON.stringify(joinData);
+        // Note: The only way of determining whether we are in the middle of
+        // hand right now is whether or not finding the hand returns null.
+        let hand = hands.find(h => h.username === players[i].username);
+        if (hand) hand = hand.hand;
 
-          // Broadcast to rest of clients that someone joined
-          wss.clients.forEach(function(client) {
-            if (client !== ws) {
-              client.send(joinDataSerialized);
-            }
-          });
-        }
+        const gatewayData = {
+          players,
+          actionIndex,
+          pot,
+          hand,
+          contributions,
+          board,
+          stage: stages[stageIndex],
+          player: players[i],
+          position: getPosition(i)
+        };
+
+        client.emit('gateway-success', gatewayData);
         break;
+      }
+    }
 
-      // This is for return visits in the same session (i.e. refresh browser)
-      case 'gateway':
-        let playerFound = false;
+    if (!playerFound) {
+      client.emit('gateway-failure');
+    }
+  });
+
+  client.on('ready', function(username) {
+    for (const p of players) {
+      if (p.username === username) {
+        p.ready = true;
+      }
+    }
+
+    // If everyone is ready, initialize a pot and deal out some hands
+    if (!players.some(p => p.ready === false)) {
+      stageIndex++;
+
+      smallBlindIndex = bigBlindIndex + 1;
+      if (smallBlindIndex === players.length) {
+        smallBlindIndex = 0;
+      }
+
+      // Post small and big blinds. Blinds hard coded to 50-100 for now (no ante)
+      for (let i = 0; i < players.length; i++) {
+        if (i === bigBlindIndex) {
+          players[i].stack -= 100;
+          contributions.push({ amount: 100, username: players[i].username, type: 'big-blind' });
+        }
+        else if (i === smallBlindIndex) {
+          players[i].stack -= 50;
+          // Unshifting this because the bets need to ordered smallest to greatest
+          contributions.unshift({ amount: 50, username: players[i].username, type: 'small-blind' });
+        }
+      }
+      pot = 150;
+
+      // This is where we emit the 'hand' event to all clients.
+      // Maybe it should be called 'start-hand'?
+      let hand;
+      let s;
+      for (const k in io.sockets.connected) {
+        s = io.sockets.connected[k];
+        console.log('Current socket username:', s.username);
         for (let i = 0; i < players.length; i++) {
-          if (players[i].username === messageData.username) {
+          if (players[i].username === s.username) {
 
-            // Note: The only way of determining whether we are in the middle of
-            // hand right now is whether or not finding the hand returns null.
-            let hand = hands.find(h => h.username === players[i].username);
-            if (hand) hand = hand.hand;
+            // Just playing Texas Hold 'em for now. (Starting with 2 cards)
+            hand = [extractRandomCard(deck), extractRandomCard(deck)];
+            hands.push({ username: players[i].username, hand });
 
-            const gatewayData = {
+            // Action will always start out left of the big blind
+            actionIndex = bigBlindIndex - 1;
+            if (actionIndex < 0) {
+              actionIndex = players.length - 1;
+            }
+
+            s.emit('start-hand', {
+              hand,
               players,
               actionIndex,
               pot,
-              hand,
-              bets,
+              contributions,
+              stage: stages[stageIndex],
               player: players[i],
-              position: getPosition(i),
-              type: 'gateway'
-            };
-            ws.send(JSON.stringify(gatewayData));
-
-            playerFound = true;
+              position: getPosition(i)
+            });
             break;
           }
         }
-
-        if (!playerFound) {
-          const gatewayFailureData = {
-            type: 'gateway-failure',
-            msg: `Could not find player with username ${messageData.username}`
-          };
-          ws.send(JSON.stringify(gatewayFailureData));
-        }
-        break;
-
-      case 'ready':
-        for (const p of players) {
-          if (p.username === messageData.username) {
-            p.ready = true;
-          }
-        }
-
-        // If everyone is ready, initialize a pot and deal out some hands
-        if (!players.some(p => p.ready === false)) {
-          smallBlindIndex = bigBlindIndex + 1;
-          if (smallBlindIndex === players.length) {
-            smallBlindIndex = 0;
-          }
-
-          // Post small and big blinds. Blinds hard coded to 50-100 for now (no ante)
-          for (let i = 0; i < players.length; i++) {
-            if (i === bigBlindIndex) {
-              players[i].stack -= 100;
-              bets.push({ amount: 100, username: players[i].username });
-            }
-            else if (i === smallBlindIndex) {
-              players[i].stack -= 50;
-              // Unshifting this because the bets need to ordered smallest to greatest
-              bets.unshift({ amount: 50, username: players[i].username });
-            }
-          }
-          pot = 150;
-
-          // This is where we emit the 'hand' event to all clients.
-          // Maybe it should be called 'start-hand'?
-          let hand;
-          wss.clients.forEach(function(client) {
-            for (let i = 0; i < players.length; i++) {
-              if (players[i].username === client.username) {
-
-                // Just playing Texas Hold 'em for now. (Starting with 2 cards)
-                hand = [extractRandomCard(deck), extractRandomCard(deck)];
-                hands.push({ username: players[i].username, hand });
-
-                // Action will always start out left of the big blind
-                actionIndex = bigBlindIndex - 1;
-                if (actionIndex < 0) {
-                  actionIndex = players.length - 1;
-                }
-
-                client.send(JSON.stringify({
-                  hand,
-                  players,
-                  actionIndex,
-                  pot,
-                  bets,
-                  type: 'hand',
-                  player: players[i],
-                  position: getPosition(i)
-                }));
-                break;
-              }
-            }
-          });
-        }
-        break;
-
-      case 'raise':
-        players[actionIndex].stack -= messageData.amount;
-        pot += messageData.amount
-        bets.push({ amount: messageData.amount, username: messageData.username });
-
-        actionIndex++;
-        if (actionIndex === players.length) actionIndex = 0;
-
-        const raiseData = JSON.stringify({ players, pot, bets, actionIndex, type: 'raise' });
-        wss.clients.forEach(function(client) {
-          client.send(raiseData);
-        });
-        break;
-
-      default:
-        console.error('Unrecognized message type:', messageData.type);
+      }
     }
   });
+
+  client.on('raise', function({ amount, username }) {
+    players[actionIndex].stack -= amount;
+    pot += amount
+    contributions.push({ amount, username, type: 'raise' });
+
+    incrementActionIndex();
+
+    const raiseData = {
+      players,
+      pot,
+      contributions,
+      actionIndex,
+      player: players[actionIndex]
+    };
+
+    io.sockets.emit('raise', raiseData);
+  });
+
+  client.on('call', function({ username, amountToCall }) {
+    players[actionIndex].stack -= amountToCall;
+    pot += amountToCall;
+    contributions.push({ username, amount: amountToCall, type: 'call' });
+
+    incrementActionIndex();
+
+    const callData = { players, pot, contributions };
+
+    let latestBettor;
+    for (const c of contributions) {
+      if (c.type === 'bet' || c.type === 'raise' || c.type === 'big-blind') {
+        latestBettor = c.username;
+      }
+    }
+
+    // Find out whether action index is same as last bettor?
+    if (players[actionIndex].username === latestBettor) {
+      // In the body of this we are moving to the next 'street'
+
+      // This shouldn't get bigger than or equal to the stages array size
+      // because the 'call' message type should never be received when in
+      // 'showdown' stage.
+      stageIndex++;
+      contributions = [];
+      callData.contributions = [];
+
+      actionIndex = bigBlindIndex + 1;
+      if (actionIndex >= players.length) actionIndex = 0;
+
+      if (board.length === 0) {
+        for (let i = 0; i < 3; i++) {
+          board.push(extractRandomCard(deck));
+        }
+      }
+      else if (board.length === 3) {
+        board.push(extractRandomCard(deck));
+      }
+      else if (board.length === 4) {
+        board.push(extractRandomCard(deck));
+      }
+      else if (board.length === 5) {
+        // TODO: Showdown
+      }
+
+      callData.actionIndex = actionIndex;
+      callData.board = board;
+      callData.stage = stages[stageIndex];
+    }
+
+    io.sockets.emit('call', callData);
+  });
+});
+
+server.listen(8080, function() {
+  console.log('Listening on port 8080...');
 });
 
 // Position is 0 if big blind, 1 if small blind, 2, if dealer,
@@ -201,4 +249,9 @@ function getPosition(index) {
   else {
     return players.length - bigBlindIndex + index;
   }
+}
+
+function incrementActionIndex() {
+  actionIndex++;
+  if (actionIndex === players.length) actionIndex = 0;
 }
